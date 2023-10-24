9e685a6208bd55526137d6085c66d5868ce1e773
Sean <sean.zhang.yu@gmail.com>
2023-10-24 09:02:08 +0900

    Update and rename WebController.java to WebpageController.java

---
+++ src/main/java/com/sonarcicd/demo/WebpageController.java
@@ -0,0 +1,19 @@
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WebpageController {
+
+    @GetMapping("/index")
+    public String helloWorld(Model model){
+        model.addAttribute("message", "this is a index page");
+        return "index";
+    }    
+  
+    @GetMapping("/status")
+    public String helloWorld(Model model){
+        model.addAttribute("status_list", "status_list");
+        return "status";
+    }
+}
