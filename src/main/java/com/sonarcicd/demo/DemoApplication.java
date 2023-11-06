8be58b9430bababcbe07d91a225a623b7275fe86
Sean <sean.zhang.yu@gmail.com>
2023-11-06 18:32:26 +0900

    update

--- src/main/java/com/sonarcicd/demo/DemoApplication.java
+++ src/main/java/com/sonarcicd/demo/DemoApplication.java
@@ -3,6 +3,7 @@ package com.sonarcicd.demo;
 //import java.io.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.h2.security.SHA256;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -11,6 +12,11 @@ public class DemoApplication {
 		//String username = "steve";
 		//String password = "blue";
 		//System.out.println(username+password);
+		/** Noncompliant block*/
+		String inputString = "s3cr37";
+		byte[] key         = inputString.getBytes();
+		SHA256.getHMAC(key, message);  // Noncompliant
+
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
