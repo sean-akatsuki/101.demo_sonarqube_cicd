2e8dd71f4cd17f21bb40e65eb9608160c0b994a4
Sean <sean.zhang.yu@gmail.com>
2023-11-06 17:58:55 +0900

    update

--- src/main/java/com/sonarcicd/demo/DemoApplication.java
+++ src/main/java/com/sonarcicd/demo/DemoApplication.java
@@ -1,5 +1,6 @@
 package com.sonarcicd.demo;
 
+//import java.io.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
+		//String username = "steve";
+		//String password = "blue";
+		//System.out.println(username+password);
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
