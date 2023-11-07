476c14b12ce219682fd6215f38c54c4c2c4464f2
Sean <sean.zhang.yu@gmail.com>
2023-11-07 09:12:36 +0900

    update

--- src/main/java/com/sonarcicd/demo/DemoApplication.java
+++ src/main/java/com/sonarcicd/demo/DemoApplication.java
@@ -2,16 +2,11 @@ package com.sonarcicd.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.h2.security.SHA256;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		String inputString = "s3cr37";
-		byte[] key= inputString.getBytes();
-		byte[] message="message".getBytes(); 
-		SHA256.getHMAC(key, message); 
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
